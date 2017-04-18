c25264eb5f77f8707f6b0b10877b94b0a3e59d95
Guillaume.Grosshenny <guillaume.grosshenny@etu.unistra.fr>
2017-04-18 18:10:38 +0200

    Modification du fichier Joueur.java.

--- Classes/Joueur.java
+++ Classes/Joueur.java
@@ -1,19 +1,1 @@
-/**
- * interface Joueur
- * Interface for JoueurImpl
- **/
 
- import java.rmi.Remote ;
- import java.rmi.RemoteException ;
-
-public interface Joueur extends Remote
-{
-  public int steal(int rscType, int amount)
-    throws RemoteException;
-
-  public void gameIsOver(int winnerID)
-    throws RemoteException;
-
-  public void begin()
-    throws RemoteException;
-}
