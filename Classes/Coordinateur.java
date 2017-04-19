092c023009c40250df2aa0c353aa86238956815a
Azranoth <awasmer96@yahoo.fr>
2017-04-19 15:40:03 +0200

    update Coordinateur

--- Classes/Coordinateur.java
+++ Classes/Coordinateur.java
@@ -9,58 +9,45 @@ public class Coordinateur {
     public Coordinateur() {}
 
 	public static void endGame(int idJoueur){
-		for(int i = 0; i < agents.length(); i++){
+		for(int i = 0; i < Joueurs.length(); i++){
 			
-			agents[i].gameIsOver(idJoueur - nb_producteurs);
+			Joueurs[i].gameIsOver(idJoueur);
 		}
+		for(int i = 0; i < Producteurs.length(); i++){
+			for(int j = 0; j < Producteurs[i].length(); j++){
+				Producteurs[i][j].gameIsOver(idJoueur);
 	}
         
     public static void main(String args[]) {
 		
 		if (args.length != 4)
-    {
-		System.out.println("Usage : java Client <machine du Serveur> <port du rmiregistry> <nb_Joueurs> <nb_Producteurs") ;
-		System.exit(0) ;
-    }
+		{
+			System.out.println("Usage : java Coordinateur <machine du Serveur> <port du rmiregistry> <liste_Joueurs> <liste_Producteurs") ;
+			System.exit(0) ;
+		}
+		
+		Joueur[] Joueurs;
+		ProducteurServeur[][] Producteurs;
+		
         
         try {
-            Coordinateur obj = new Coordinateur();
-            Agent stub = (Agent) UnicastRemoteObject.exportObject(obj, 0);
-            
-            int nb_joueurs = args[3];
-            int nb_producteurs = args[4];
-            Agent[] agents;
-            Ressource[] objectives;
-			Ressource[] objectives_player;
-            Random rand = new Random();
-            int randNbRessources = rand.nextInt(nb_producteurs + 1);
-			int randRessourceID_prod;
+            int i;
+            for(i = 0; i < args[2].length(); i++){
+				Joueurs[i] = naming.lookup("rmi://" + args[0] + ":" + args[1] + "/" + args[2][i]);
+			}
+			for(i = 0; i < args[3].length(); i++){
+				Producteurs[args[3][i].production.getType()][Producteurs[args[3][i].production.getType()].length()] = naming.lookup("rmi://" + args[0] + ":" + args[1] + "/" + args[3][i]);
+			}
 			
-			for(int i = 0; i < randNbRessources; i++){
-				objectives[i] = new Ressource(i, 0, 100);
+			for(i = 0; i < Joueurs.length(); i++){
+				Joueurs[i].SendLists(Joueurs, Producteurs);
 			}
-            
-            for(int i = 0; i < nb_joueurs + nb_producteurs; i++){
-					if(i <= nb_producteurs){
-						/* Déterminer aléatoirement quelle ressource lui attribuer (fuck eduroam) */
-						if(i <= randNbRessources){ // On s'assure qu'il y ait au moins 1 producteur pour chaque ressource
-							agents[i] = new Producteur(i, 0, /*addr*/, /*coord*/, objectives[i] );
-						}
-						else{
-							randRessourceID_prod = rand.nextInt(randNbRessources);
-							agents[i] = new Producteur(i, 0, /*addr*/, /*coord*/, objectives[randRessourceID_prod] );
-						}
-					}
-					else{
-						for(int j = 0; j < randNbRessources; j++){
-							objectives_player[j] = objectives[j].copy();
-						}
-						agents[i] = new JoueurImpl(i, 1, /*addr*/, /*coord*/, false, objectives_player, nb_joueurs);
-					}						
+			for(i = 0; i < Producteurs.length(); i++){
+				for(int j = 0; j < Producteurs[i].length(); j++){
+					Producteurs[i][j].SendLists(Joueurs, Producteurs);
+				}
 			}
-
-            // Bind the remote object's stub in the registry
-           Naming.rebind( "rmi://localhost:" + args[0] + "/JoueurImpl" ,stub) ;
+			
            System.err.println("Server ready");
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
