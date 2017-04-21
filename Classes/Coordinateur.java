30c11871eb9f5455166ca84e593d1de3a669d372
Guillaume.Grosshenny <guillaume.grosshenny@etu.unistra.fr>
2017-04-21 15:45:38 +0200

    Nettoyage du code de Coordinateur.

--- Classes/Coordinateur.java
+++ Classes/Coordinateur.java
@@ -14,43 +14,14 @@ public class Coordinateur {
 
 		IJoueur tempJoueur = null;
 		Producteur tempProd = null;
-		/*
-		int nb_players = Integer.parseInt(args[2]);
-		int nb_producers = Integer.parseInt(args[3]);
-		String[] Joueurs = new String[nb_players];
-		String[][] Producteurs = new String[nb_producers][nb_producers];
-		*/
-        try {
-			/*
-            int i;
-            int j = 0;
-            for(i = 0; i < nb_players; i++){
-				Joueurs[i] = "rmi://" + args[0] + ":" + args[1] + "/" + args[4+i];
-			}
-			for(i = 0; i < nb_producers; i++){
-				try {
-					System.out.println("avant : " + "rmi://" + args[0] + ":" + args[1] + "/" + args[4+nb_players+i]);
-					tempProd = (Producteur) Naming.lookup("rmi://" + args[0] + ":" + args[1] + "/" + args[4+nb_players+i]);
-					System.out.println("avant : " + Producteurs[tempProd.getTypeOfRsc()].length + " , " );
-					while(Producteurs[tempProd.getTypeOfRsc()][j] != null){
-						j++;
-					}
-					Producteurs[tempProd.getTypeOfRsc()][j] = "rmi://" + args[0] + ":" + args[1] + "/" + args[4+nb_players+i];
-					System.out.println("Apres");
-				}
-				catch (NotBoundException re) { System.out.println(re) ; }
-				catch (RemoteException re) { System.out.println(re) ; }
-				catch (MalformedURLException e) { System.out.println(e) ; }
-
-			}
-			*/
-			
+    try {
 			CoordinateurImpl Coord = new CoordinateurImpl(args);
 			Naming.rebind(args[args.length - 1], Coord);
-			
+
 			int i, j;
-			
-			for(i = 0; i < Coord.Players.length; i++){
+
+			for(i = 0; i < Coord.Players.length; i++)
+      {
 				System.out.println("JOUEURS : " + Coord.Players[i]);
 			}
 			for(i = 0; i < Coord.Producers.length; i++){
