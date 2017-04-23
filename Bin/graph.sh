d7bdfb11d02ee9e3df314e2a1b898ea69a187599
Azranoth <awasmer96@yahoo.fr>
2017-04-23 22:16:18 +0200

    shell de mes burnes

--- Bin/graph.sh
+++ Bin/graph.sh
@@ -2,20 +2,23 @@
 
 if [ $# -ne 2 ]
 then
-	echo "usage : $0 <idJoueur> <nbTypeOfRsc>"
+	echo 'usage : $0 <idJoueur> <nbTypeOfRsc>'
 	exit 1
 fi
 
-filename = "log_file_"
-logfilename = ${filename}$1
-nbRsc = $2
+filename='log_file_'
+logfilename=${filename}$1
+nbRsc=$2
 
 if [ -f $logfilename ]
 then
-	plot for [IDX=0:nbRsc] $logfilename u 1:nbRsc with lines lt IDX+1
+	cat << __EOF | gnuplot 
+	set term canvas mousing size 500,500 
+	plot <$logfilename> u 1:2 with lines lt IDX+1 
+	__EOF
 else
-	echo "File not found"
+	echo 'File not found'
 	exit 1
 fi
-
+exit 0
 
