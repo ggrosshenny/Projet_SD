5ae2a30f64230d7a80e2ddeb299821c10c768407
Azranoth <awasmer96@yahoo.fr>
2017-04-29 00:50:15 +0200

    update generation graph : sous-répertoires de logs pour chaque partie; gestion des logfiles joueur/producteur

--- Bin/graph.sh
+++ Bin/graph.sh
@@ -2,22 +2,35 @@
 
 
 # Vérification du nombre d'arguments
-if [ $# -ne 2 ]
+# option -pl : logfile d'un joueur
+# option -pr : logfile d'un producteur
+if [ $# -ne 4 ]
 then
-	echo 'usage : '$0' <idJoueur> <nbTypeOfRsc>'
+	echo 'usage : '$0' [-pr || -pl] <idGame> <idJoueur> <nbTypeOfRsc>'
 	exit 1
 fi
 
 # On récupère le nom du fichier de logs à modéliser
-filename='../logs/log_player_'
-logfilename=${filename}$1".bat"
-nbRsc=$2
+if [ $1 = -pr ]
+then
+	filename='../logs/logs_game_'${2}'/log_producer_'
+else
+	if [ $1 = -pl ]
+	then
+		filename='../logs/logs_game_'${2}'/log_player_'
+	else
+		echo 'usage : '$0' [pr || pl] <idGame> <idJoueur> <nbTypeOfRsc>'
+		exit 1
+	fi
+fi
+logfilename=${filename}$3".bat"
+nbRsc=$4
 
 # Si le fichier de logs existe, alors pour chaque bloc de données de ce fichier on trace une courbe dans un fichier .png grâce à gnuplot
 if [ -f $logfilename ]
 then
-	outputfile=${filename}$1".png"
-	echo "set term png; set output '"${outputfile}"'; set title 'player "$1" logs'; set key outside; plot for [IDX=0:"$nbRsc"-1] '"${logfilename}"' index IDX u 1:2 title columnheader(1) with lines lt IDX" | gnuplot
+	outputfile=${filename}$3".png"
+	echo "set term png; set output '"${outputfile}"'; set title 'player "$3" logs'; set key outside; plot for [IDX=0:"$nbRsc"-1] '"${logfilename}"' index IDX u 1:2 title columnheader(1) with lines lt IDX" | gnuplot
 	see ${outputfile} &
 else
 	echo 'Logfile not found'
--- Classes/LogWriter.java
+++ Classes/LogWriter.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.nio.file.*;
-
+import java.io.File;
 
 /**
 * Class : LogWriter
@@ -38,45 +38,69 @@ public class LogWriter {
 	public void writeLogFiles(){
 		
 		PrintWriter logfile;
-		// For each player create a new log file
-		for(int i = 0; i < players_logs.size(); i++) {			
-			try {				
-				logfile = new PrintWriter("log_player_"+(i+1)+".bat", "UTF-8");
-				// For each ressource create a block
-				for(int j = 0; j < players_logs.get(i).size(); j++){
-					logfile.println('"' + "Ressource " + j + '"');
-					
-					for(int k = 0; k < players_logs.get(i).get(j).size(); k++){
-						// Write a new line in the block with as first column the time elapsed since the beginning
-						// of the game and as second column the ressource amount owned by the player at this time
-						logfile.println((timeBetweenTicks + timeBetweenTicks*k) + " " + players_logs.get(i).get(j).get(k));
-					}
-					// Gnuplot requires 2 blank lines separating each blocks
-					logfile.println("");
-					logfile.println("");
-				}
-				// Close the logfile & move it to the logs repository
-				logfile.close();
-				Files.move(Paths.get("../Bin/log_player_"+(i+1)+".bat"), Paths.get("../logs/log_player_"+(i+1)+".bat"));
-			
-			}
-			catch (IOException e) { System.out.println(e); }
-			catch (UnsupportedOperationException e) { System.out.println(e); }
+		int idx = 0;
+		boolean creationDirDone = false;
+		
+		// Search for the last game index
+		File gameLogDir = new File("../logs/logs_game_" + idx);
+		while(gameLogDir.exists()){
+			idx++;
+			gameLogDir = new File("../logs/logs_game_" + idx);
 		}
 		
-		// For each producer create a new log file
-		for(int i = 0; i < producers_logs.size(); i++){
-			try {
-				logfile = new PrintWriter("log_producer_"+i, "UTF-8");
-				// Write a new line with as first colum the time elapsed since the beginning of the game and
-				// as second column the ressource amount owned by the producer at this time
-				for(int j = 0; j < producers_logs.get(i).size(); j++){
-					logfile.println((timeBetweenTicks + timeBetweenTicks*j) + "	" + producers_logs.get(i).get(j));
-				}
-				logfile.close();
-			}
-			catch (IOException e) { System.out.println(e); }
+		// Then try to create a new directory (index + 1)
+		try{
+			gameLogDir.mkdir();
+			creationDirDone = true;
 		}
+		catch (SecurityException se) { System.out.println(se); }
+		
+		if(!creationDirDone){
+			System.out.println ("Error : game logs directory cannot be created");
+		}
+		else{	
+		
+			// For each player create a new log file
+			for(int i = 0; i < players_logs.size(); i++) {			
+				try {				
+					logfile = new PrintWriter("log_player_"+(i+1)+".bat", "UTF-8");
+					// For each ressource create a block
+					for(int j = 0; j < players_logs.get(i).size(); j++){
+						logfile.println('"' + "Ressource " + j + '"');
+						
+						for(int k = 0; k < players_logs.get(i).get(j).size(); k++){
+							// Write a new line in the block with as first column the time elapsed since the beginning
+							// of the game and as second column the ressource amount owned by the player at this time
+							logfile.println((timeBetweenTicks + timeBetweenTicks*k) + " " + players_logs.get(i).get(j).get(k));
+						}
+						// Gnuplot requires 2 blank lines separating each blocks
+						logfile.println("");
+						logfile.println("");
+					}
+					// Close the logfile & move it to the logs repository
+					logfile.close();
+					Files.move(Paths.get("../Bin/log_player_"+(i+1)+".bat"), Paths.get("../logs/"+gameLogDir+"/log_player_"+(i+1)+".bat"));
 				
+				}
+				catch (IOException e) { System.out.println(e); }
+				catch (UnsupportedOperationException e) { System.out.println(e); }
+			}
+			
+			// For each producer create a new log file
+			for(int i = 0; i < producers_logs.size(); i++){
+				try {
+					logfile = new PrintWriter("log_producer_"+i, "UTF-8");
+					// Write a new line with as first colum the time elapsed since the beginning of the game and
+					// as second column the ressource amount owned by the producer at this time
+					for(int j = 0; j < producers_logs.get(i).size(); j++){
+						logfile.println((timeBetweenTicks + timeBetweenTicks*j) + "	" + producers_logs.get(i).get(j));
+					}
+					logfile.close();
+					Files.move(Paths.get("../Bin/log_producer_"+(i+1)+".bat"), Paths.get("../logs/"+gameLogDir+"/log_producer_"+(i+1)+".bat"));
+				}
+				catch (IOException e) { System.out.println(e); }
+			}
+					
+		}
 	}
 }
