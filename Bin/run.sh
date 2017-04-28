975d345500efa9c7795ee5e46eb8059e5372c0b5
Azranoth <awasmer96@yahoo.fr>
2017-04-28 22:38:50 +0200

    update du script run - ajout du support TbT, joueurs Indiv, joueurs humains

--- Bin/run.sh
+++ Bin/run.sh
@@ -2,9 +2,9 @@
 # Grosshenny Guillaume & Wasmer Audric
 
 # Verification des arguments
-if [ $# -ne 7 ]
+if [ $# -ne 11 ]
 then
-    echo "usage : $0 <port> <nbProd> <nbJoueurs> <nbTypeOfRsc> <AmountAtStart> <AmountToTakeForVictory> <TimeBeforeProduction>"
+    echo "usage : $0 <port> <nbProd> <nbJoueurs> <nbTypeOfRsc> <AmountAtStart> <AmountToTakeForVictory> <TimeBeforeProduction> <isTurnByTurn> <nbJoueursCoop> <nbJoueursIndiv> <nbJoueursHumains>"
     exit 1
 fi
 
@@ -14,6 +14,21 @@ then
     echo "Warning, the number of different types of ressource is greater than the number of producers. Please, retry with a correct producers number or a correct type of ressource number."
     exit 1
 fi
+
+# Verification des arguments 3 : nombre de joueurs
+if [ `expr ${9} + ${10} + ${11}` -ne $3 ]
+then
+	echo 'Warning, total number of Coop & Indiv & Human players is greater than the actual number of players passed as the third argument.'
+	exit 1
+fi
+
+# Si au moins un joueur humain est annoncé, la partie passe automatique en tour-par-tour
+TbT=$8
+if [ ${11} -gt 0 ]
+then
+	TbT=1
+fi
+
 
 # Creation du rmi registry
 
@@ -69,15 +84,28 @@ done
 echo $rscTypeCtrlList
 
 # Création des joueurs
+idxTypeJoueur=0
 for i in $PlayerIDList
 do
-  xterm -e java Joueur localhost $1 $i 0 $Coordinateur 0 0 $4 &
+  if [ $idxTypeJoueur -lt $8 ]
+  then
+	xterm -e java Joueur localhost $1 $i 0 $Coordinateur 0 $TbT $4 0 &
+    idxTypeJoueur=`expr $idxTypeJoueur + 1`
+  else
+    if [ $idxTypeJoueur -lt `expr $9 + $10` ]
+    then
+      xterm -e java Joueur localhost $1 $i 0 $Coordinateur 1 $TbT $4 0 &
+      idxTypeJoueur=`expr $idxTypeJoueur + 1`
+    else
+      xterm -e java Joueur localhost $1 $i 0 $Coordinateur 0 $TbT $4 1 &
+    fi    
+  fi
 done
 
 
 # Création du coordinateur de la partie
 sleep 2
-java Coordinateur localhost $1 $3 $2 $PlayerIDList $ProdIDList $Coordinateur 0 $4 &
+java Coordinateur localhost $1 $3 $2 $PlayerIDList $ProdIDList $Coordinateur $TbT $4 &
 echo "Done."
 
 exit 0
